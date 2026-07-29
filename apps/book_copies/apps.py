faaa2db1e415a588df6dab3a115d7f9ad010052c
swafey-karanja <kswafey@gmail.com>
2026-07-29 17:17:17 +0300

    created the book_copies app and added more functionalities to the existing apps

--- apps/book_copies/apps.py
+++ apps/book_copies/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class BookCopiesConfig(AppConfig):
-    name = 'book_copies'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.book_copies"
+    label = "book_copies"
